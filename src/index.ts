2cb19dafbc13ca6d299ac1dbddb1c3009819079a
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-14 13:48:35 -0300

    feat: configurando aplicação para upload de imagem

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { server } from "./server/Server";
 import 'dotenv/config'
 import { tryConnection } from "./server/database/MongoConnect";
+import { eventsRouter } from "./routes/EventsRouter";
+import bodyParser from "body-parser";
 
 
 const port = process.env.PORT || 5000;
@@ -8,5 +10,10 @@ const port = process.env.PORT || 5000;
 server.listen(3333, () => {
     console.log('Servidor rodando na porta: ' + port);
 });
+
+server.use(eventsRouter);
+
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
 
 tryConnection();
