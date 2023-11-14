bc0da1828a6183dc6b94a526f5ec917ad8ecef3f
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-14 13:49:49 -0300

    feat: criando tipos para as entidades

---
+++ @types/types.ts
@@ -0,0 +1,34 @@
+export type TUser = {
+    email: string,
+    name: string,
+    age: number,
+    password: string,
+    address: string,
+    educationLevel: string
+}
+
+export type TFeedback = {
+    date: Date,
+    user: TUser,
+    content: string
+}
+
+export type TEvent = {
+    name: string,
+    description: string,
+    theme: string,
+    organizingCommitte: string,
+    startDate: Date,
+    endDate: Date,
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    location:  {
+        type: 'Point',
+        coordinates: number[]
+    },
+    feedbacks: TFeedback[]
+}
+
+
