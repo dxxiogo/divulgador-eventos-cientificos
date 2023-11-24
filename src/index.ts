d86935363d7b2bae6e43a9f45100407a22ef81ca
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-24 13:16:57 -0300

    feat: configurando cors

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import { tryConnection } from "./server/database/MongoConnect";
 import { eventsRouter } from "./routes/EventsRouter";
 import bodyParser from "body-parser";
-
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
@@ -11,9 +11,12 @@ server.listen(3333, () => {
     console.log('Servidor rodando na porta: ' + port);
 });
 
+server.use(cors());
+
 server.use(eventsRouter);
 
 server.use(bodyParser.urlencoded({ extended: false }))
+
 server.use(bodyParser.json())
 
 tryConnection();
