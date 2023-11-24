6e0d806aad22fb3c888d8f458a7d2d1d281f6d16
Andre-lucs <andreltd312@gmail.com>
2023-11-24 10:46:49 -0300

    refactor:Error handling for database connection

--- src/server/database/MongoConnect.ts
+++ src/server/database/MongoConnect.ts
@@ -4,8 +4,13 @@ import 'dotenv/config'
 tryConnection().catch(err => console.log(err));
 
 export async function tryConnection() {
-  if(process.env.MONGO_DATABASE_URL)
-    await mongoose.connect(process.env.MONGO_DATABASE_URL);
-    console.log('Conectado ao Mongo!')
+  if(process.env.MONGO_DATABASE_URL){
+    await mongoose.connect(process.env.MONGO_DATABASE_URL).then(() =>{
+      console.log('Conectado ao Mongo!')
+    }).catch((err) => {
+      console.log('Erro ao conectar ao Mongo: ' + err)
+    });
+
+  }
 }
 
